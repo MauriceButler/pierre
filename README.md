b6c577ae98dc2a568b3a2655c1fffa17239ab759
mauricebutler <maurice.butler@gmail.com>
2014-03-17 13:38:24 +1000

    clean up naming / spelling issues

--- README.md
+++ README.md
@@ -5,18 +5,18 @@ A waiter module. Wait for this id to be served then run this function
 
 ## Usage
 
-    var pieree = require('pieree');
+    var pierre = require('pierre');
 
-    pieree.waitFor('1234', function(){
+    pierre.waitFor('1234', function(){
         console.log('1234 happened');
     });
 
-    pieree.ready('1234');
+    pierre.ready('1234');
 
-    pieree.waitFor('5678', function(arg1, arg2, arg3){
+    pierre.waitFor('5678', function(arg1, arg2, arg3){
         console.log('5678 recieved arguments:', arg1, arg2, arg3);
     });
 
-    pieree.ready('5678', 'foo', 'bar', 'meh');
+    pierre.ready('5678', 'foo', 'bar', 'meh');
 
 
--- tests/tests.js
+++ tests/tests.js
@@ -1,4 +1,4 @@
-var test = require('tape'),
+var test = require('grape'),
     pathToObjectUnderTest = '../index';
 
 function getCleanTestObject(request){
@@ -7,86 +7,86 @@ function getCleanTestObject(request){
     return objectUnderTest;
 }
 
-test('pieree Exists', function (t) {
+test('pierre Exists', function (t) {
     t.plan(2);
-    var pieree = getCleanTestObject();
-    t.ok(pieree, 'pieree Exists');
-    t.equals(typeof pieree, 'object',  'pieree is an object');
+    var pierre = getCleanTestObject();
+    t.ok(pierre, 'pierre Exists');
+    t.equal(typeof pierre, 'object',  'pierre is an object');
 });
 
-test('pieree.waitFor Exists', function (t) {
+test('pierre.waitFor Exists', function (t) {
     t.plan(2);
-    var pieree = getCleanTestObject();
-    t.ok(pieree.waitFor, 'pieree.waitFor Exists');
-    t.equals(typeof pieree.waitFor, 'function',  'pieree.waitFor is an function');
+    var pierre = getCleanTestObject();
+    t.ok(pierre.waitFor, 'pierre.waitFor Exists');
+    t.equal(typeof pierre.waitFor, 'function',  'pierre.waitFor is an function');
 });
 
-test('pieree.ready Exists', function (t) {
+test('pierre.ready Exists', function (t) {
     t.plan(2);
-    var pieree = getCleanTestObject();
-    t.ok(pieree.ready, 'pieree.ready Exists');
-    t.equals(typeof pieree.ready, 'function',  'pieree.ready is an function');
+    var pierre = getCleanTestObject();
+    t.ok(pierre.ready, 'pierre.ready Exists');
+    t.equal(typeof pierre.ready, 'function',  'pierre.ready is an function');
 });
 
 test('multiple callbacks are called', function (t) {
     t.plan(2);
-    var pieree = getCleanTestObject();
+    var pierre = getCleanTestObject();
 
-    pieree.waitFor('1234', function(){
+    pierre.waitFor('1234', function(){
         t.pass('1234 happened');
     });
-    pieree.waitFor('1234', function(){
+    pierre.waitFor('1234', function(){
         t.pass('1234 happened again');
     });
 
-    pieree.ready('1234');
+    pierre.ready('1234');
 });
 
 test('callbacks are cleared', function (t) {
     t.plan(1);
-    var pieree = getCleanTestObject();
+    var pierre = getCleanTestObject();
 
-    pieree.waitFor('1234', function(){
+    pierre.waitFor('1234', function(){
         t.pass('1234 happened only once');
     });
 
-    pieree.ready('1234');
-    pieree.ready('1234');// does nothing
+    pierre.ready('1234');
+    pierre.ready('1234');// does nothing
 });
 
 test('only correct callbacks are called', function (t) {
     t.plan(1);
-    var pieree = getCleanTestObject();
+    var pierre = getCleanTestObject();
 
-    pieree.waitFor('1234', function(){
+    pierre.waitFor('1234', function(){
         t.pass('1234 happened');
     });
 
-    pieree.waitFor('5678', function(){
+    pierre.waitFor('5678', function(){
         t.fail('5678 happened');
     });
 
-    pieree.ready('1234');
+    pierre.ready('1234');
 });
 
 
 test('additional arguments are passed to callback', function (t) {
     t.plan(6);
-    var pieree = getCleanTestObject(),
+    var pierre = getCleanTestObject(),
         testData = { test: 'data'};
 
-    pieree.waitFor('1234', function(data){
-        t.equals(data, testData, 'data was passed');
+    pierre.waitFor('1234', function(data){
+        t.equal(data, testData, 'data was passed');
         t.pass('1234 happened');
     });
 
-    pieree.waitFor('5678', function(){
-        t.equals(arguments.length, 3, 'all arguments were passed');
-        t.equals(arguments[0], 'foo', 'first argument is correct');
-        t.equals(arguments[1], 'bar', 'second argument is correct');
-        t.equals(arguments[2], 'meh', 'third argument is correct');
+    pierre.waitFor('5678', function(){
+        t.equal(arguments.length, 3, 'all arguments were passed');
+        t.equal(arguments[0], 'foo', 'first argument is correct');
+        t.equal(arguments[1], 'bar', 'second argument is correct');
+        t.equal(arguments[2], 'meh', 'third argument is correct');
     });
 
-    pieree.ready('1234', testData);
-    pieree.ready('5678', 'foo', 'bar', 'meh');
+    pierre.ready('1234', testData);
+    pierre.ready('5678', 'foo', 'bar', 'meh');
 });
